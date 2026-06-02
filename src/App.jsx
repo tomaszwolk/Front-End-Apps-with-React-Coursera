115d366bee096bddefbf6c72fe3d859380eb2390
tomaszwolk <wolk.tomasz@gmail.com>
2026-06-02 10:41:51 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App(props) {
+  const currDate = new Date();
+
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>
+        It is {currDate.toLocaleDateString()} The time now is
+        {currDate.toLocaleTimeString()}.
+      </h2>
+    </div>
+  );
+}
+
+export default App;
